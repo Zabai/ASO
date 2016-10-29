6ad77b988032e42fc3e5a0a6351be6e96e11fdba
Zabai Armas Herrera <Zabai@HostASO.(none)>
2016-10-29 18:44:32 +0100

    Añadido el tercer script

---
+++ Scripts/3. Finder.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function finder(){
+	index=0
+	find /home -exec stat -c "%U" {} \;
+}
+
+function printer(){
+	uniq -c | while read count user
+	do
+		if(( $count >= $1 ))
+		then
+			echo "$user"
+		fi
+	done
+}
+
+if(( $# == 1 ))
+then
+	nFiles=$1
+	
+	echo "Lista de usuarios con mas de $nFiles ficheros"
+	finder | printer $nFiles
+else
+	echo Nº incorrecto de argumentos
+fi
