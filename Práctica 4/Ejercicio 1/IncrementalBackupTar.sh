16dbf5f76ea0a3c00505c06a9b195eb3665544e8
Zabai Armas <zbrmas@hotmail.com>
2016-11-19 18:38:24 +0000

    Corrección Práctica 4

--- Práctica 4/Ejercicio 1/IncrementalBackupTar.sh
+++ Práctica 4/Ejercicio 1/IncrementalBackupTar.sh
@@ -3,6 +3,6 @@
 backupDir=/Backups
 date=$(date +"%Y%m%d")
 
-find ~prueba ~invitado -newer "$backupDir/$(ls $backupDir -tr | egrep '*0.tgz' | tail -1)" | tar czf "$backupDir/$date-1.tgz" -T -
+find ~prueba ~invitado -type f -newer "$backupDir/$(ls $backupDir -tr | egrep '*0.tgz' | tail -1)" | tar czf "$backupDir/$date-1.tgz" -T -
 
 exit 0
